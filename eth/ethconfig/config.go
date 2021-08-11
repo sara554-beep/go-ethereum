0434608945aaf9da7c7778a41ab6e9be0d783047
rjl493456442 <garyrong0905@gmail.com>
2021-08-11 10:53:26 +0800

    update

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -80,7 +80,7 @@ var Defaults = Config{
 	TrieCleanCacheJournal:   "triecache",
 	TrieCleanCacheRejournal: 60 * time.Minute,
 	TrieDirtyCache:          1024,
-	TrieTimeout:             30 * time.Second,
+	TrieTimeout:             120 * time.Second,
 	SnapshotCache:           102,
 	Miner: miner.Config{
 		GasFloor: 8000000,
