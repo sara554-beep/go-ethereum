ff7116543f874168624b4f9504d600c979bae145
Gary Rong <garyrong0905@gmail.com>
2022-01-12 13:41:25 +0800

    eth, miner: rebase

--- eth/catalyst/api.go
+++ eth/catalyst/api.go
@@ -258,7 +258,7 @@ func (api *ConsensusAPI) assembleBlock(parentHash common.Hash, params *PayloadAt
 	if err != nil {
 		return nil, err
 	}
-	return BlockToExecutableData(block, params.Random), nil
+	return BlockToExecutableData(block), nil
 }
 
 func encodeTransactions(txs []*types.Transaction) [][]byte {
@@ -313,7 +313,9 @@ func ExecutableDataToBlock(params ExecutableDataV1) (*types.Block, error) {
 	return block, nil
 }
 
-func BlockToExecutableData(block *types.Block, random common.Hash) *ExecutableDataV1 {
+// BlockToExecutableData constructs the executableDataV1 structure by filling the
+// fields from the given block. It assumes the given block is post-merge block.
+func BlockToExecutableData(block *types.Block) *ExecutableDataV1 {
 	return &ExecutableDataV1{
 		BlockHash:     block.Hash(),
 		ParentHash:    block.ParentHash(),
@@ -327,7 +329,7 @@ func BlockToExecutableData(block *types.Block, random common.Hash) *ExecutableDa
 		ReceiptsRoot:  block.ReceiptHash(),
 		LogsBloom:     block.Bloom().Bytes(),
 		Transactions:  encodeTransactions(block.Transactions()),
-		Random:        random,
+		Random:        block.MixDigest(),
 		ExtraData:     block.Extra(),
 	}
 }
