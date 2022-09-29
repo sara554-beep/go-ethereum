a5691cb47d5ed8e8dc43a336a96eacd6cb8a76ed
Gary Rong <garyrong0905@gmail.com>
2022-09-29 15:39:32 +0800

    all: implement path-based state scheme

--- core/rawdb/accessors_state.go
+++ core/rawdb/accessors_state.go
@@ -17,6 +17,8 @@
 package rawdb
 
 import (
+	"encoding/binary"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/log"
@@ -26,6 +28,17 @@ import (
 func ReadPreimage(db ethdb.KeyValueReader, hash common.Hash) []byte {
 	data, _ := db.Get(preimageKey(hash))
 	return data
+}
+
+// WritePreimages writes the provided set of preimages to the database.
+func WritePreimages(db ethdb.KeyValueWriter, preimages map[common.Hash][]byte) {
+	for hash, preimage := range preimages {
+		if err := db.Put(preimageKey(hash), preimage); err != nil {
+			log.Crit("Failed to store trie preimage", "err", err)
+		}
+	}
+	preimageCounter.Inc(int64(len(preimages)))
+	preimageHitCounter.Inc(int64(len(preimages)))
 }
 
 // ReadCode retrieves the contract code of the provided code hash.
@@ -45,12 +58,6 @@ func ReadCode(db ethdb.KeyValueReader, hash common.Hash) []byte {
 // will only check the existence with latest scheme(with prefix).
 func ReadCodeWithPrefix(db ethdb.KeyValueReader, hash common.Hash) []byte {
 	data, _ := db.Get(codeKey(hash))
-	return data
-}
-
-// ReadTrieNode retrieves the trie node of the provided hash.
-func ReadTrieNode(db ethdb.KeyValueReader, hash common.Hash) []byte {
-	data, _ := db.Get(hash.Bytes())
 	return data
 }
 
@@ -74,34 +81,10 @@ func HasCodeWithPrefix(db ethdb.KeyValueReader, hash common.Hash) bool {
 	return ok
 }
 
-// HasTrieNode checks if the trie node with the provided hash is present in db.
-func HasTrieNode(db ethdb.KeyValueReader, hash common.Hash) bool {
-	ok, _ := db.Has(hash.Bytes())
-	return ok
-}
-
-// WritePreimages writes the provided set of preimages to the database.
-func WritePreimages(db ethdb.KeyValueWriter, preimages map[common.Hash][]byte) {
-	for hash, preimage := range preimages {
-		if err := db.Put(preimageKey(hash), preimage); err != nil {
-			log.Crit("Failed to store trie preimage", "err", err)
-		}
-	}
-	preimageCounter.Inc(int64(len(preimages)))
-	preimageHitCounter.Inc(int64(len(preimages)))
-}
-
 // WriteCode writes the provided contract code database.
 func WriteCode(db ethdb.KeyValueWriter, hash common.Hash, code []byte) {
 	if err := db.Put(codeKey(hash), code); err != nil {
 		log.Crit("Failed to store contract code", "err", err)
-	}
-}
-
-// WriteTrieNode writes the provided trie node database.
-func WriteTrieNode(db ethdb.KeyValueWriter, hash common.Hash, node []byte) {
-	if err := db.Put(hash.Bytes(), node); err != nil {
-		log.Crit("Failed to store trie node", "err", err)
 	}
 }
 
@@ -112,9 +95,87 @@ func DeleteCode(db ethdb.KeyValueWriter, hash common.Hash) {
 	}
 }
 
-// DeleteTrieNode deletes the specified trie node from the database.
-func DeleteTrieNode(db ethdb.KeyValueWriter, hash common.Hash) {
-	if err := db.Delete(hash.Bytes()); err != nil {
-		log.Crit("Failed to delete trie node", "err", err)
+// ReadTrieHistory retrieves the trie history with the given id. Calculate
+// the real position of trie history in freezer by minus one since the first
+// history object is started from one(zero for empty state).
+func ReadTrieHistory(db ethdb.AncientReaderOp, id uint64) []byte {
+	blob, err := db.Ancient(trieHistoryTable, id-1)
+	if err != nil {
+		return nil
+	}
+	return blob
+}
+
+// WriteTrieHistory writes the provided trie history to database. Calculate the
+// real position of trie history in freezer by minus one since the first history
+// object is started from one(zero is not existent corresponds to empty state).
+func WriteTrieHistory(db ethdb.AncientWriter, id uint64, blob []byte) {
+	db.ModifyAncients(func(op ethdb.AncientWriteOp) error {
+		op.AppendRaw(trieHistoryTable, id-1, blob)
+		return nil
+	})
+}
+
+// ReadStateLookup retrieves the state id with the provided state root.
+func ReadStateLookup(db ethdb.KeyValueReader, root common.Hash) (uint64, bool) {
+	data, err := db.Get(stateLookupKey(root))
+	if err != nil || len(data) == 0 {
+		return 0, false
+	}
+	return binary.BigEndian.Uint64(data), true
+}
+
+// WriteStateLookup writes the provided state lookup to database.
+func WriteStateLookup(db ethdb.KeyValueWriter, root common.Hash, id uint64) {
+	var buff [8]byte
+	binary.BigEndian.PutUint64(buff[:], id)
+	if err := db.Put(stateLookupKey(root), buff[:]); err != nil {
+		log.Crit("Failed to store state lookup", "err", err)
+	}
+}
+
+// DeleteStateLookup deletes the specified state lookup from the database.
+func DeleteStateLookup(db ethdb.KeyValueWriter, root common.Hash) {
+	if err := db.Delete(stateLookupKey(root)); err != nil {
+		log.Crit("Failed to delete state lookup", "err", err)
+	}
+}
+
+// ReadHeadState retrieves the id of the disk state from the database.
+func ReadHeadState(db ethdb.KeyValueReader) uint64 {
+	data, _ := db.Get(headStateKey)
+	if len(data) != 8 {
+		return 0
+	}
+	return binary.BigEndian.Uint64(data)
+}
+
+// WriteHeadState stores the id of the disk state into database.
+func WriteHeadState(db ethdb.KeyValueWriter, number uint64) {
+	if err := db.Put(headStateKey, encodeBlockNumber(number)); err != nil {
+		log.Crit("Failed to store the head state id", "err", err)
+	}
+}
+
+// ReadTrieJournal retrieves the serialized in-memory trie node diff layers saved at
+// the last shutdown. The blob is expected to be max a few 10s of megabytes.
+func ReadTrieJournal(db ethdb.KeyValueReader) []byte {
+	data, _ := db.Get(triesJournalKey)
+	return data
+}
+
+// WriteTrieJournal stores the serialized in-memory trie node diff layers to save at
+// shutdown. The blob is expected to be max a few 10s of megabytes.
+func WriteTrieJournal(db ethdb.KeyValueWriter, journal []byte) {
+	if err := db.Put(triesJournalKey, journal); err != nil {
+		log.Crit("Failed to store tries journal", "err", err)
+	}
+}
+
+// DeleteTrieJournal deletes the serialized in-memory trie node diff layers saved at
+// the last shutdown
+func DeleteTrieJournal(db ethdb.KeyValueWriter) {
+	if err := db.Delete(triesJournalKey); err != nil {
+		log.Crit("Failed to remove tries journal", "err", err)
 	}
 }
